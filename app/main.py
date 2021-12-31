642edd571f38842289375daee1031f5ea5d4cfc3
cdw8431 <cdw8431@gmail.com>
2022-01-01 00:37:02 +0900

    ci: app start shell script 추가

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import uvicorn
 from api.router import api_router
 from config import settings
 from fastapi import FastAPI
@@ -23,6 +22,3 @@ def redirect_docs() -> Response:
 
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
